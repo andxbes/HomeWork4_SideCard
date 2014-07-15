c9abb3c777fb64d2ae143b9bbf664fff32a36b47
Andr <you@example.com>
2014-07-15 17:16:07 +0300

    Merge origin/Testing into Testing
    Conflicts:
    	test/testing/TestingIT.java

--- src/main/java/ua/bescenniy/sitecard_homework4/config/MvcConfiguration.java
+++ src/main/java/ua/bescenniy/sitecard_homework4/config/MvcConfiguration.java
@@ -20,13 +20,11 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter{
 		resolver.setPrefix("/WEB-INF/views/");
 		resolver.setSuffix(".jsp");
 		return resolver;
-		
 	}
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
-		
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");	
 	}
 
 	
